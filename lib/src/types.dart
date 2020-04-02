99ddbad27339d0a0061b58b0e6039d1163b9fc06
hanjingjing <jingjing.han@justalk.com>
2020-04-02 14:33:04 +0800

    添加类型

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -1,5 +1,5 @@
 /// The available intervals for periodically showing notifications
-enum RepeatInterval { EveryMinute, Hourly, Daily, Weekly }
+enum RepeatInterval { EveryMinute, Hourly, Daily, Weekly, CustomDaily }
 
 /// Details of a pending notification that has not been delivered
 class PendingNotificationRequest {
